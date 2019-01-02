16c0d31b2397f2232862f81e737d3243f435f962
Vadivel <mvadivel1@NTS203789MC.local>
2019-01-02 23:25:02 +0530

    update

--- dags/email_sample.py
+++ dags/email_sample.py
@@ -1,3 +1,16 @@
+
+"""
+Do proper smtp configuration in airflow.cfg file
+
+[smtp]
+smtp_host = smtp.gmail.com
+smtp_starttls = True
+smtp_ssl = False
+smtp_user = abc@gmail.com
+smtp_port = 587
+smtp_password = password@1234567
+smtp_mail_from = abc@gmail.com
+"""
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
@@ -5,29 +18,35 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.email_operator import EmailOperator
 
 def print_hello():
-    return 'Hello world!'
+    print("Hello")
 
 default_args = {
-        'owner': 'shahina',
+        'owner': 'airflow',
         'start_date':datetime(2018,8,11),
-        'email': ['starnn.sapient@gmail.com', 'manojkumar.vsj@gmail.com'],
+        'email': ['abc@gmail.com'],
         'email_on_failure': True,
         'email_on_retry': True,
 }
 
-dag = DAG('email_sample', 
-          description='Simple tutorial DAG',
-          schedule_interval='* * * * *',
-          default_args = default_args, 
+dag = DAG('email_sample',
+          description='Simple email task',
+          schedule_interval='*/5 * * * *',
+          default_args = default_args,
           catchup=False)
 
-dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
+dummy_operator = DummyOperator(
+    task_id='dummy_task',
+    retries=3,
+    dag=dag)
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+hello_operator = PythonOperator(
+    task_id='hello_task',
+    python_callable=print_hello,
+    dag=dag)
 
 email = EmailOperator(
         task_id='send_email',
-        to='starnn.sapient@gmail.com,manojkumar.vsj@gmail.com',
+        to='abcdef@gmail.com,abc@gmail.com',
         subject='Airflow Alert',
         html_content=""" <h3>Email Test</h3> """,
         dag=dag
